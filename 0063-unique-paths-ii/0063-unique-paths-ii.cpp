2f7f6fd728d8f0dcb1a723c8da18a26a238d2f22
Hemant Kumar <hemantkumar90089h@gmail.com>
2026-09-09 22:51:41 +0530

    Time: 0 ms (100%), Space: 11.9 MB (31.13%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,23 +1,36 @@
 class Solution {
 private:
-    int func(int i, int j, vector<vector<int>>& matrix, vector<vector<int>> &dp){
-        if (i < 0 || j < 0 || matrix[i][j] == 1) return 0;
-        else if(i == 0 && j == 0) return 1;
+    int func(int m, int n, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
 
-        if(dp[i][j] != -1) return dp[i][j];
+                int up = 0;
+                int left = 0;
+                if (i > 0)
+                    up = dp[i - 1][j];
+                if (j > 0)
+                    left = dp[i][j - 1];
 
-        int up = func(i - 1, j, matrix, dp);
-        int left = func(i, j - 1, matrix, dp);
-
-        return dp[i][j] = up + left;
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m - 1][n - 1];
     }
-    
+
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1)); 
+        int m = matrix.size();   
+        int n = matrix[0].size(); 
+        vector<vector<int>> dp(m, vector<int>(n, 0));
 
-        return func(m-1, n-1, matrix, dp);
+        return func(m, n, matrix, dp);
     }
 };
