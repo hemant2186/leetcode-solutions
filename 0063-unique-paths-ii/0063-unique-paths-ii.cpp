abb7a96b30d679fcf57320254100e8153b14eea9
Hemant Kumar <hemantkumar90089h@gmail.com>
2026-09-09 22:53:02 +0530

    Time: 1 ms (15.48%), Space: 11.5 MB (90.44%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,36 +1,38 @@
 class Solution {
 private:
-    int func(int m, int n, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
+    int func(int m, int n, vector<vector<int>>& matrix) {
+        vector<int> prev(n, 0), curr(n, 0);
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 1) {
-                    dp[i][j] = 0;
+                    curr[j] = 0;
                     continue;
                 }
                 if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
+                    curr[j] = 1;
                     continue;
                 }
 
                 int up = 0;
                 int left = 0;
                 if (i > 0)
-                    up = dp[i - 1][j];
+                    up = prev[j];
                 if (j > 0)
-                    left = dp[i][j - 1];
-
-                dp[i][j] = up + left;
+                    left = curr[j - 1];
+                curr[j] = up + left;
             }
+            
+            prev = curr;
         }
-        return dp[m - 1][n - 1];
+        return prev[n-1];
     }
 
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& matrix) {
         int m = matrix.size();   
         int n = matrix[0].size(); 
-        vector<vector<int>> dp(m, vector<int>(n, 0));
 
-        return func(m, n, matrix, dp);
+        return func(m, n, matrix);
     }
 };
